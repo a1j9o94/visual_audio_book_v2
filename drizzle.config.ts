23681826fa80f8456d346c5b1eafb9f29853f5a6
Adrian Obleton <obletonadrian@gmail.com>
2024-11-06 14:32:56 -0500

    initial commit

---
+++ drizzle.config.ts
@@ -0,0 +1,12 @@
+import { type Config } from "drizzle-kit";
+
+import { env } from "~/env";
+
+export default {
+  schema: "./src/server/db/schema.ts",
+  dialect: "sqlite",
+  dbCredentials: {
+    url: env.DATABASE_URL,
+  },
+  tablesFilter: ["visual_audio_book_v2_*"],
+} satisfies Config;
